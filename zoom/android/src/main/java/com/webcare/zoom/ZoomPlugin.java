b497daa1cdf0b81e7d344c23c1ee13a16bfe7d26
C.M Talha <47864926+CM-Talha@users.noreply.github.com>
2021-10-03 23:32:42 -0700

    fixed import issue

--- zoom/android/src/main/java/com/webcare/zoom/ZoomPlugin.java
+++ zoom/android/src/main/java/com/webcare/zoom/ZoomPlugin.java
@@ -20,6 +20,7 @@ import us.zoom.sdk.JoinMeetingOptions;
 import us.zoom.sdk.JoinMeetingParams;
 import us.zoom.sdk.MeetingService;
 import us.zoom.sdk.MeetingStatus;
+import us.zoom.sdk.MeetingViewsOptions;
 import us.zoom.sdk.StartMeetingOptions;
 import us.zoom.sdk.StartMeetingParamsWithoutLogin;
 import us.zoom.sdk.ZoomError;
@@ -143,8 +144,7 @@ public class ZoomPlugin implements FlutterPlugin, MethodCallHandler,ActivityAwar
         opts.no_dial_in_via_phone = parseBoolean(options, "disableDialIn", false);
         opts.no_disconnect_audio = parseBoolean(options, "noDisconnectAudio", false);
         opts.no_audio = parseBoolean(options, "noAudio", false);
-        opts.meeting_views_options=MeetingViewsOptions.NO_TEXT_MEETING_ID
-                + MeetingViewsOptions.NO_TEXT_PASSWORD;
+        opts.meeting_views_options = MeetingViewsOptions.NO_BUTTON_SHARE + MeetingViewsOptions.NO_TEXT_MEETING_ID + MeetingViewsOptions.NO_TEXT_PASSWORD;
         JoinMeetingParams params = new JoinMeetingParams();
 
         params.displayName = options.get("userId");
